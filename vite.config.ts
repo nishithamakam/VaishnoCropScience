5d2f9dccc7becbb89de5149f53a9d018d3a2472f
nishithamakam <nishithamakam23@gmail.com>
2025-09-21 21:04:13 +0530

    Added gh-pages for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -19,6 +19,7 @@ export default defineConfig({
         ]
       : []),
   ],
+  base: '/VaishnoCropScience/', 
   resolve: {
     alias: {
       "@": path.resolve(import.meta.dirname, "client", "src"),
